f877457b8c26b199b9b138da72fdb91476d36d00
memo <mbirgi@gmail.com>
2025-01-04 11:43:49 +0100

    add log separator and emphasize script start message

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ os.makedirs(os.path.dirname(log_file_path), exist_ok=True)
 logging.basicConfig(filename=log_file_path, level=logging.INFO, 
                     format='%(asctime)s - %(levelname)s - %(message)s')
 
+logging.info("====================")
 logging.info("**** Starting script")
 
 # login
